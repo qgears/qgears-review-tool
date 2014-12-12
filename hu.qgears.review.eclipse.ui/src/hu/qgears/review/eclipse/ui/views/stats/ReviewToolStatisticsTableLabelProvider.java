901e7719b2b74ce1c59fa504b05ec1b84c672531
agostoni <agostoni@users.noreply.github.com>
2014-12-12 18:17:21 +0100

    Statistic table supports ordering (see #8)

--- hu.qgears.review.eclipse.ui/src/hu/qgears/review/eclipse/ui/views/stats/ReviewToolStatisticsTableLabelProvider.java
+++ hu.qgears.review.eclipse.ui/src/hu/qgears/review/eclipse/ui/views/stats/ReviewToolStatisticsTableLabelProvider.java
@@ -5,10 +5,12 @@ import hu.qgears.review.report.ReportEntry;
 import hu.qgears.review.report.ReportGenerator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.graphics.Image;
 
 /**
@@ -20,6 +22,21 @@ import org.eclipse.swt.graphics.Image;
 public class ReviewToolStatisticsTableLabelProvider extends LabelProvider implements ITableLabelProvider{
 
 	private List<ColumnDefinition> columns = new ReportGenerator(null, null).getColumnDefinitions();
+	private int sortColumnIndex;
+	private boolean direction;
+	private ViewerSorter sorter = new ViewerSorter (){
+		public int compare(org.eclipse.jface.viewers.Viewer viewer, Object e1, Object e2) {
+			if (sortColumnIndex >= 0 && sortColumnIndex < columns.size()){
+				ColumnDefinition cd = columns.get(sortColumnIndex); 
+				Comparator<ReportEntry> cmp = cd.getComparator();
+				if (e1 instanceof ReportEntry && e2 instanceof ReportEntry){
+					return (direction ? 1 : -1) * cmp.compare((ReportEntry)e1,(ReportEntry) e2);
+				}
+			}
+			return 0;
+		};
+	};
+	
 	@Override
 	public Image getColumnImage(Object element, int columnIndex) {
 		return null;
@@ -45,9 +62,74 @@ public class ReviewToolStatisticsTableLabelProvider extends LabelProvider implem
 	 */
 	public List<String> getColumnNames() {
 		List<String> cols = new ArrayList<String>();
+		int index = 0;
 		for (ColumnDefinition cd : columns){
-			cols.add(cd.getTitle());
+			String title = cd.getTitle();
+			if (index == sortColumnIndex){
+				title = title + (getDirection() ? " ↑" : " ↓");
+			}
+			cols.add( title);
+			index++;
 		}
 		return cols;
+	}
+
+	/**
+	 * Returns the viewer sorter that is able to order elements in associated
+	 * table.
+	 * 
+	 * @return
+	 */
+	public ViewerSorter getSorter() {
+		return sorter;
+	}
+
+	/**
+	 * Returns the header text for specifed column
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public String getHeaderText(int index) {
+		List<String> columnNames = getColumnNames();
+		if (columnNames.size() > index && index >= 0){
+			return columnNames.get(index);
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Updates comparator, the table content will be ordered by specified column index.
+	 * 
+	 * @param selectedColumn
+	 */
+	public void update(int selectedColumn){
+		if (sortColumnIndex == selectedColumn){
+			direction = !direction;
+		} else {
+			sortColumnIndex = selectedColumn;
+			direction = false;
+		}
+	}
+
+	/**
+	 * Returns the index of column, which determines the sorting order
+	 * currently.
+	 * 
+	 * @return
+	 */
+	public int getSortColumnIndex() {
+		return sortColumnIndex;
+	}
+	
+	/**
+	 * <code>true</code> means ascending order, <code>false</code> means
+	 * descending order.
+	 * 
+	 * @return
+	 */
+	public boolean getDirection(){
+		return direction;
 	}
 }
--- hu.qgears.review.eclipse.ui/src/hu/qgears/review/eclipse/ui/views/stats/ReviewToolStatisticsView.java
+++ hu.qgears.review.eclipse.ui/src/hu/qgears/review/eclipse/ui/views/stats/ReviewToolStatisticsView.java
@@ -17,6 +17,8 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -34,7 +36,8 @@ import org.eclipse.ui.views.properties.IPropertySheetPage;
  * @author agostoni
  * 
  */
-public class ReviewToolStatisticsView extends ViewPart implements  ISelectionListener{
+public class ReviewToolStatisticsView extends ViewPart implements
+		ISelectionListener {
 
 	/**
 	 * Action that forces reloading table input.
@@ -47,15 +50,15 @@ public class ReviewToolStatisticsView extends ViewPart implements  ISelectionLis
 		private StatisticsTableInput input;
 
 		public ReloadStatisticsAction(Object input) {
-			if (input != null && input instanceof StatisticsTableInput){
-				this.input = (StatisticsTableInput)input;
+			if (input != null && input instanceof StatisticsTableInput) {
+				this.input = (StatisticsTableInput) input;
 			}
 			updateState();
 		}
 
 		private void updateState() {
-			if (input != null){
-				setText("Reload statistics of "+input.getSourceSet());
+			if (input != null) {
+				setText("Reload statistics of " + input.getSourceSet());
 				setEnabled(true);
 			} else {
 				setEnabled(false);
@@ -64,7 +67,8 @@ public class ReviewToolStatisticsView extends ViewPart implements  ISelectionLis
 
 		@Override
 		public void run() {
-			statisticsTable.setInput(new StatisticsTableInput(input.getReviewInstance(), input.getSourceSet(),true));
+			statisticsTable.setInput(new StatisticsTableInput(input
+					.getReviewInstance(), input.getSourceSet(), true));
 		}
 	}
 
@@ -75,26 +79,43 @@ public class ReviewToolStatisticsView extends ViewPart implements  ISelectionLis
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout());
 		statisticsTable = createTableViewer(parent);
-		getViewSite().getWorkbenchWindow().getSelectionService().addSelectionListener(this);
+		getViewSite().getWorkbenchWindow().getSelectionService()
+				.addSelectionListener(this);
 		createContextMenus();
 	}
 
 	protected TableViewer createTableViewer(Composite parent) {
-		TableViewer viewer = new TableViewer(parent);
-		ReviewToolStatisticsTableLabelProvider p = new ReviewToolStatisticsTableLabelProvider();
-		for ( String col : p.getColumnNames()){
+		final TableViewer viewer = new TableViewer(parent);
+		final ReviewToolStatisticsTableLabelProvider p = new ReviewToolStatisticsTableLabelProvider();
+		int columnIndex = 0;
+		for (String col : p.getColumnNames()) {
 			TableViewerColumn tc = new TableViewerColumn(viewer, SWT.NONE);
-			TableColumn c = tc.getColumn();
+			final TableColumn c = tc.getColumn();
 			c.setText(col);
 			c.setWidth(100);
 			c.setMoveable(true);
+			final int index = columnIndex;
+			tc.getColumn().addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					p.update(index);
+					int i = 0;
+					for (TableColumn c :viewer.getTable().getColumns()){
+						c.setText(p.getHeaderText(i++));
+					}
+					viewer.refresh();
+				}
+			});
+			columnIndex++;
 		}
 		viewer.getTable().setHeaderVisible(true);
 		viewer.setLabelProvider(p);
+		viewer.setSorter(p.getSorter());
 		ReviewToolStatisticsTableContentProvider provider = new ReviewToolStatisticsTableContentProvider();
 		viewer.setContentProvider(provider);
 		getSite().setSelectionProvider(provider);
-		viewer.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL,true, true));
+		viewer.getTable().setLayoutData(
+				new GridData(SWT.FILL, SWT.FILL, true, true));
 		return viewer;
 	}
 
@@ -105,19 +126,25 @@ public class ReviewToolStatisticsView extends ViewPart implements  ISelectionLis
 
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (!selection.isEmpty() && selection instanceof StructuredSelection){
+		if (!selection.isEmpty() && selection instanceof StructuredSelection) {
 			StructuredSelection sel = (StructuredSelection) selection;
 			Object sourceSetCandidate = sel.getFirstElement();
-			if (sourceSetCandidate instanceof ReviewSourceSetView){
-				ReviewInstance riCandidate = (ReviewInstance) part.getAdapter(ReviewInstance.class);
-				if (riCandidate != null){
-					statisticsTable.setInput(new StatisticsTableInput(riCandidate, ((ReviewSourceSetView) sourceSetCandidate).getModelElement(),false));
+			if (sourceSetCandidate instanceof ReviewSourceSetView) {
+				ReviewInstance riCandidate = (ReviewInstance) part
+						.getAdapter(ReviewInstance.class);
+				if (riCandidate != null) {
+					statisticsTable.setInput(new StatisticsTableInput(
+							riCandidate,
+							((ReviewSourceSetView) sourceSetCandidate)
+									.getModelElement(), false));
 				}
-			} else if (sourceSetCandidate instanceof SourceTreeElement){
-				ReviewInstance riCandidate = (ReviewInstance) part.getAdapter(ReviewInstance.class);
+			} else if (sourceSetCandidate instanceof SourceTreeElement) {
+				ReviewInstance riCandidate = (ReviewInstance) part
+						.getAdapter(ReviewInstance.class);
 				SourceTreeElement ste = (SourceTreeElement) sourceSetCandidate;
-				if (riCandidate != null){
-					statisticsTable.setInput(new StatisticsTableInput(riCandidate, ste.getSet(),false));
+				if (riCandidate != null) {
+					statisticsTable.setInput(new StatisticsTableInput(
+							riCandidate, ste.getSet(), false));
 				}
 				selectReportEntry(ste);
 			}
@@ -132,18 +159,21 @@ public class ReviewToolStatisticsView extends ViewPart implements  ISelectionLis
 	 *            The review source.
 	 */
 	private void selectReportEntry(SourceTreeElement ste) {
-		if (statisticsTable.getInput() instanceof StatisticsTableInput){
-			StatisticsTableInput ip = (StatisticsTableInput) statisticsTable.getInput();
-			if (ip.getReport() != null){
-				for (ReportEntry re : ip.getReport()){
-					if (re.getSourceFile() == ste.getSource()){
-						statisticsTable.setSelection(new StructuredSelection(re),true);
+		if (statisticsTable.getInput() instanceof StatisticsTableInput) {
+			StatisticsTableInput ip = (StatisticsTableInput) statisticsTable
+					.getInput();
+			if (ip.getReport() != null) {
+				for (ReportEntry re : ip.getReport()) {
+					if (re.getSourceFile() == ste.getSource()) {
+						statisticsTable.setSelection(
+								new StructuredSelection(re), true);
 						break;
 					}
 				}
 			}
 		}
 	}
+
 	protected void createContextMenus() {
 		IMenuListener listener = new IMenuListener() {
 			@Override
@@ -151,34 +181,36 @@ public class ReviewToolStatisticsView extends ViewPart implements  ISelectionLis
 				fillContextMenu(manager);
 			}
 		};
-		getViewSite().getActionBars().getMenuManager().addMenuListener(listener);
+		getViewSite().getActionBars().getMenuManager()
+				.addMenuListener(listener);
 		fillContextMenu(getViewSite().getActionBars().getMenuManager());
-		
+
 		MenuManager viewMenu = new MenuManager();
 		fillContextMenu(viewMenu);
 		viewMenu.addMenuListener(listener);
-		statisticsTable.getControl().setMenu(viewMenu.createContextMenu(statisticsTable.getControl()));
+		statisticsTable.getControl().setMenu(
+				viewMenu.createContextMenu(statisticsTable.getControl()));
 		getSite().registerContextMenu(viewMenu, statisticsTable);
 	}
-	
+
 	private void fillContextMenu(IMenuManager manager) {
 		manager.setRemoveAllWhenShown(true);
 		manager.add(new RefreshViewerAction(statisticsTable));
 		manager.add(new ReloadStatisticsAction(statisticsTable.getInput()));
 	}
+
 	@Override
 	public void dispose() {
-		getViewSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(this);
+		getViewSite().getWorkbenchWindow().getSelectionService()
+				.removeSelectionListener(this);
 		getSite().setSelectionProvider(null);
 	}
-	
+
 	@Override
 	public Object getAdapter(@SuppressWarnings("rawtypes") Class adapter) {
-		if (adapter.equals(IPropertySheetPage.class)){
-			if(propertySheetPage==null)
-			{
-				propertySheetPage = 
-					new ReviewToolPropertyPage();
+		if (adapter.equals(IPropertySheetPage.class)) {
+			if (propertySheetPage == null) {
+				propertySheetPage = new ReviewToolPropertyPage();
 			}
 			return propertySheetPage;
 		}

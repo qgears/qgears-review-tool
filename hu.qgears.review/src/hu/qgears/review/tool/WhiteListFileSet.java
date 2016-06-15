06d806d4730df0c47a5eeb2e4b413b874b812ee6
agostoni <agostoni@users.noreply.github.com>
2016-06-15 16:05:08 +0200

    Support exlude patterns in whitelist file set (lines starting with '! ' are considered as exlusion patterns)

--- hu.qgears.review/src/hu/qgears/review/tool/WhiteListFileSet.java
+++ hu.qgears.review/src/hu/qgears/review/tool/WhiteListFileSet.java
@@ -18,7 +18,9 @@ public class WhiteListFileSet {
 
 	public static final Object ID = "whitelistfileset";
 	private static final String COMMENT_MARK = "#";
-	private List<Matcher> validPatterns = new ArrayList<Matcher>();
+	private static final String EXCLUSION_MARK = "! ";
+	private List<Matcher> inclusionPatterns = new ArrayList<Matcher>();
+	private List<Matcher> exclusionPatterns = new ArrayList<Matcher>();
 	
 	/**
 	 * Creates a new instance. See {@link WhiteListFileSet head comment}
@@ -34,8 +36,17 @@ public class WhiteListFileSet {
 	private void parseValidPatterns(List<String> lines) {
 		for (String l : lines){
 			l = l.trim();
-			if (!ID.equals(l) && !l.startsWith(COMMENT_MARK) && !l.isEmpty()){
-				validPatterns.add(Pattern.compile(l).matcher(""));
+			if (!l.isEmpty() && !ID.equals(l)) {
+				if (l.startsWith(COMMENT_MARK)) {
+					//nothing to do, this line is commented by the used
+				} else if (l.startsWith(EXCLUSION_MARK)) {
+					//exclusion pattern found
+					String pattern = l.substring(EXCLUSION_MARK.length());
+					exclusionPatterns.add(Pattern.compile(pattern).matcher(""));
+				} else {
+					//normal inclusion pattern found
+					inclusionPatterns.add(Pattern.compile(l).matcher(""));
+				}
 			}
 		}
 	}
@@ -50,13 +61,27 @@ public class WhiteListFileSet {
 	 */
 	public List<String> reduce(final List<String> stringList) {
 		List<String> mList = new ArrayList<String>();
-		for (String s : stringList){
-			for (Matcher m : validPatterns){
+		for (String s : stringList) {
+			boolean matches = false;
+			//first find out whether this line matches at least one line
+			for (Matcher m : inclusionPatterns){
 				m.reset(s);
 				if (m.matches()){
-					mList.add(s);
+					matches = true;
 					break;
 				}
+			}
+			//if matches, than check that any exclusion pattern disables the source file 
+			if (matches) {
+				for (Matcher ex : exclusionPatterns){
+					if (ex.reset(s).matches()){
+						matches = false;
+						break;
+					}
+				}
+			}
+			if (matches){
+				mList.add(s);
 			}
 		}
 		return mList;
